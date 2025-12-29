15fa8005c55495835b0fccc20faa7bb42a340217
rconsoIe <defnotjohn13@gmail.com>
2025-12-29 15:27:18 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,16 +1,39 @@
 local Packets = {}
-Packets.version = "main"
+
+Packets.version = nil
+Packets._loaded = false
 
 function Packets.init()
-    local url = string.format(
-        "https://raw.githubusercontent.com/rconsoIe/Packets/@%s/main.lua",
-        Packets.version
-    )
+    if Packets._loaded then
+        return Packets
+    end
 
-    local impl = loadstring(game:HttpGet(url))()
+    local version = Packets.version
+    local path
+
+    if version and version ~= "" then
+        path = "@%s/main.lua"
+        path = string.format(path, version)
+    else
+        path = "main/main.lua"
+    end
+
+    local url = "https://raw.githubusercontent.com/rconsoIe/Packets/" .. path
+
+    local ok, impl = pcall(function()
+        return loadstring(game:HttpGet(url))()
+    end)
+
+    if not ok or type(impl) ~= "table" then
+        error("Packets: failed to load implementation (" .. path .. ")")
+    end
+
     for k, v in pairs(impl) do
         Packets[k] = v
     end
+
+    Packets._loaded = true
+    return Packets
 end
 
 return Packets
